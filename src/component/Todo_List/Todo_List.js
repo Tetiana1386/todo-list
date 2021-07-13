61206b9623b84ae3c9bdaf57ef615c1a468dab70
Tetiana1386 <tanyashostak13@gmail.com>
2021-07-14 01:25:10 +0300

    todo_list

---
+++ src/component/Todo_List/Todo_List.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import TodoListItem from '../TodoListItem/TodoListItem';
+
+import './Todo_List.module.css';
+
+const TodoList = ({ items, onToggleImportant, onToggleDone, onDelete }) => {
+  const elements = items.map(item => {
+    const { id, ...itemProps } = item;
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListItem
+          {...itemProps}
+          onToggleImportant={() => onToggleImportant(id)}
+          onToggleDone={() => onToggleDone(id)}
+          onDelete={() => onDelete(id)}
+        />
+      </li>
+    );
+  });
+
+  return <ul className="todo-list list-group">{elements}</ul>;
+};
+
+export default TodoList;
